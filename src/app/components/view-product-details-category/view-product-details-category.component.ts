9987b91fc51a80b1d11dd00e98cc398381598113
Snehal-Sabade <snehalsabade1947@gmail.com>
2023-01-18 23:07:08 +0530

    view-products-detail added on cart

--- src/app/components/view-product-details-category/view-product-details-category.component.ts
+++ src/app/components/view-product-details-category/view-product-details-category.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { TopDealService } from 'src/app/core/http/top-deal.service';
+import { SharedService } from 'src/app/shared/service/shared.service';
 
 @Component({
   selector: 'app-view-product-details-category',
@@ -8,28 +9,44 @@ import { TopDealService } from 'src/app/core/http/top-deal.service';
   styleUrls: ['./view-product-details-category.component.scss']
 })
 export class ViewProductDetailsCategoryComponent implements OnInit {
-  topDealByCategory :any;
-  category:any;
-  constructor(private topDealService: TopDealService,private router:Router) { }
+  topDealByCategory: any;
+  category: any;
+  constructor(private topDealService: TopDealService, private router: Router, private sharedService: SharedService) { }
 
   ngOnInit(): void {
     this.getTopDealByCategory();
   }
 
   getTopDealByCategory() {
-    this.topDealService.getDetailsFromServer('top-deals-by-category').subscribe((response:any)=>{
-      if(response && response.length > 0){
-      this.topDealByCategory=response;
+    this.topDealService.getDetailsFromServer('top-deals-by-category').subscribe((response: any) => {
+      if (response && response.length > 0) {
+        this.topDealByCategory = response;
+      }
+    })
+  }
+
+  navigateToTopDealProduct(drugCode: string) {
+    this.router.navigate(['topdealproductdetail', drugCode]);
+  }
+
+  onAddToCart(event:any,product: any) {
+    event.stopPropagation();
+    var products: any;
+    products = localStorage.getItem('products');
+    products = JSON.parse(products);
+    if (!products) {
+      products = [];
     }
-  })
-}
 
-navigateToTopDealProduct(drugCode:string){
-  this.router.navigate(['topdealproductdetail',drugCode]);
-}
+    const abc = products.find((prod:any) => prod.drugCode == product.drugCode)
+    if (abc == undefined){
+      products.push(product)
+    }else{
+      alert('Alrady in cart');
+    }
+    localStorage.setItem('products', JSON.stringify(products));
+    this.sharedService.emitSelProduct.next(products.length);
 
-onAddToCard(deal:any){
-// this.localStorage.getTopDealByCategory
-}
+  }
 
 }
